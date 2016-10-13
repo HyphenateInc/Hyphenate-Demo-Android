ada14c2266ad0f3a9e021ed61bdfbfd133ca7508
zhanw <258653667@qq.com>
2016-10-13 20:44:39 +0800

    update conversation list item style

--- app/src/main/java/com/hyphenate/chatuidemo/ui/chat/ChatActivity.java
+++ app/src/main/java/com/hyphenate/chatuidemo/ui/chat/ChatActivity.java
@@ -14,13 +14,16 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnCheckedChanged;
 import butterknife.OnClick;
+import com.hyphenate.EMMessageListener;
 import com.hyphenate.chat.EMClient;
 import com.hyphenate.chat.EMMessage;
+import com.hyphenate.chatuidemo.DemoHelper;
 import com.hyphenate.chatuidemo.R;
 import com.hyphenate.chatuidemo.ui.BaseActivity;
 import com.hyphenate.chatuidemo.ui.MainActivity;
 import com.hyphenate.easeui.EaseConstant;
 import com.hyphenate.easeui.widget.EaseChatMessageListView;
+import java.util.List;
 
 /**
  * Chat with someone in this activity
@@ -58,9 +61,27 @@ public class ChatActivity extends BaseActivity {
             }
         });
 
-
-        //init message list view
+        // init message list view
         mMessageListView.init(toChatUsername, chatType, null);
+
+        // received messages code in onResume() method
+
+    }
+
+
+    /**
+     * on send message
+     */
+    @OnClick(R.id.btn_send) void onSendMessage(){
+        if(!TextUtils.isEmpty(mContentEditText.getText())){
+            mContentEditText.setText("");
+            // create a message
+            EMMessage message = EMMessage.createTxtSendMessage(mContentEditText.getText().toString(), toChatUsername);
+            // send message
+            EMClient.getInstance().chatManager().sendMessage(message);
+            // refresh ui
+            mMessageListView.refreshSelectLast();
+        }
     }
 
     @Override public boolean onCreateOptionsMenu(Menu menu) {
@@ -78,19 +99,58 @@ public class ChatActivity extends BaseActivity {
         return true;
     }
 
+    EMMessageListener mMessageListener = new EMMessageListener() {
+        @Override public void onMessageReceived(List<EMMessage> list) {
+            for (EMMessage message : list) {
+                String username = null;
+                // group message
+                if (message.getChatType() == EMMessage.ChatType.GroupChat || message.getChatType() == EMMessage.ChatType.ChatRoom) {
+                    username = message.getTo();
+                } else {
+                    // single chat message
+                    username = message.getFrom();
+                }
 
-    /**
-     * on send message
-     */
-    @OnClick(R.id.btn_send) void onSendMessage(){
-        if(!TextUtils.isEmpty(mContentEditText.getText())){
-            EMMessage message = EMMessage.createTxtSendMessage(mContentEditText.getText().toString(), toChatUsername);
-            //send message
-            EMClient.getInstance().chatManager().sendMessage(message);
-            //refresh ui
-            mMessageListView.refreshSelectLast();
+                // if the message is for current conversation
+                if (username.equals(toChatUsername)) {
+                    mMessageListView.refreshSelectLast();
+                    //EaseUI.getInstance().getNotifier().vibrateAndPlayTone(message);
+                } else {
+                    //EaseUI.getInstance().getNotifier().onNewMsg(message);
+                }
+            }
         }
+
+        @Override public void onCmdMessageReceived(List<EMMessage> list) {
+            //cmd messages do not save to the cache in sdk
+        }
+
+        @Override public void onMessageRead(List<EMMessage> list) {
+            mMessageListView.refresh();
+        }
+
+        @Override public void onMessageDelivered(List<EMMessage> list) {
+            mMessageListView.refresh();
+        }
+
+        @Override public void onMessageChanged(EMMessage emMessage, Object o) {
+            mMessageListView.refresh();
+        }
+    };
+
+    @Override protected void onResume() {
+        super.onResume();
+        DemoHelper.getInstance().pushActivity(this);
+        // register the event listener when enter the foreground
+        // remember to remove this listener in onStop()
+        EMClient.getInstance().chatManager().addMessageListener(mMessageListener);
     }
 
-
+    @Override protected void onStop() {
+        super.onStop();
+        // unregister this event listener when this activity enters the background
+        EMClient.getInstance().chatManager().removeMessageListener(mMessageListener);
+        // remove activity from foreground activity list
+        DemoHelper.getInstance().popActivity(this);
+    }
 }
