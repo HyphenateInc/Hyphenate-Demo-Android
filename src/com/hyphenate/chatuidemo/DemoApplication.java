803a27a42322ae05c744bda14e657b6504065244
NEWUSER <jerrywu12@gmail.com>
2016-10-14 16:06:57 -0700

    update SDK to 24 install Fabric

--- src/com/hyphenate/chatuidemo/DemoApplication.java
+++ src/com/hyphenate/chatuidemo/DemoApplication.java
@@ -17,7 +17,8 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
-//import com.crashlytics.android.ndk.CrashlyticsNdk;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class DemoApplication extends Application {
 
@@ -33,7 +34,7 @@ public class DemoApplication extends Application {
 	public void onCreate() {
 		MultiDex.install(this);
 		super.onCreate();
-//		Fabric.with(this, new Crashlytics());
+		Fabric.with(this, new Crashlytics());
         applicationContext = this;
         instance = this;
         
