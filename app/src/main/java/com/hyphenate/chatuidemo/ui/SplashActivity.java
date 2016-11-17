f9ea27b8e65866731056f620cd83248f04d0af3d
zhanw <258653667@qq.com>
2016-11-17 12:31:59 +0800

    remove the sleep time

--- app/src/main/java/com/hyphenate/chatuidemo/ui/SplashActivity.java
+++ app/src/main/java/com/hyphenate/chatuidemo/ui/SplashActivity.java
@@ -3,38 +3,25 @@ package com.hyphenate.chatuidemo.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import com.hyphenate.chat.EMClient;
-import com.hyphenate.chatuidemo.call.VideoCallActivity;
-import com.hyphenate.chatuidemo.call.VoiceCallActivity;
 import com.hyphenate.chatuidemo.sign.SignInActivity;
-import com.hyphenate.util.EasyUtils;
 
 /**
  * Created by lzan13 on 2016/11/14.
  */
 
 public class SplashActivity extends BaseActivity {
-    private static final int sleepTime = 2000;
+    //private static final int sleepTime = 2000;
 
     @Override protected void onCreate(Bundle arg0) {
         super.onCreate(arg0);
         if (EMClient.getInstance().isLoggedInBefore()) {
-            String topActivityName =
-                    EasyUtils.getTopActivityName(EMClient.getInstance().getContext());
-            if (topActivityName != null && (topActivityName.equals(
-                    VideoCallActivity.class.getName()) || topActivityName.equals(
-                    VoiceCallActivity.class.getName()))) {
-                // nop
-                // avoid main screen overlap Calling Activity
-            } else {
-                //enter main screen
-                startActivity(new Intent(SplashActivity.this, MainActivity.class));
-            }
+            startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
         } else {
-            try {
-                Thread.sleep(sleepTime);
-            } catch (InterruptedException e) {
-            }
+            //try {
+            //    Thread.sleep(sleepTime);
+            //} catch (InterruptedException e) {
+            //}
             startActivity(new Intent(SplashActivity.this, SignInActivity.class));
             finish();
         }
