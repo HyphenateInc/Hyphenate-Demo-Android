da10aefe6a21c8d82856de5a9cf3c25b65fd2e5c
zhanw <258653667@qq.com>
2016-11-17 21:11:29 +0800

    support runtime permissions

--- app/src/main/java/com/hyphenate/chatuidemo/ui/MainActivity.java
+++ app/src/main/java/com/hyphenate/chatuidemo/ui/MainActivity.java
@@ -5,6 +5,7 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -34,6 +35,7 @@ import com.hyphenate.chatuidemo.chat.ConversationListFragment;
 import com.hyphenate.chatuidemo.group.GroupChangeListener;
 import com.hyphenate.chatuidemo.group.InviteMembersActivity;
 import com.hyphenate.chatuidemo.group.PublicGroupsListActivity;
+import com.hyphenate.chatuidemo.runtimepermissions.PermissionsManager;
 import com.hyphenate.chatuidemo.settings.SettingsFragment;
 import com.hyphenate.chatuidemo.sign.SignInActivity;
 import com.hyphenate.chatuidemo.user.AddContactsActivity;
@@ -500,5 +502,11 @@ public class MainActivity extends BaseActivity {
             refreshApply();
             refreshContacts();
         }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults) {
+        PermissionsManager.getInstance().notifyPermissionsChange(permissions, grantResults);
     }
 }
